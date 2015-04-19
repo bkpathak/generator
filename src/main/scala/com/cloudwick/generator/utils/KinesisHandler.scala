f3fdc7c66f2af9f1a1b7fc586deb779d27631c6d
bkpathak <bkpathak@mtu.edu>
2015-04-19 14:46:46 -0700

    kinesis handler

--- src/main/scala/com/cloudwick/generator/utils/KinesisHandler.scala
+++ src/main/scala/com/cloudwick/generator/utils/KinesisHandler.scala
@@ -1,8 +1,11 @@
 package com.cloudwick.generator.utils
 
+import io.github.cloudify.scala.aws.kinesis.Client
+import io.github.cloudify.scala.aws.auth.CredentialsProvider.DefaultHomePropertiesFile
+
 /**
  * Created by bijay on 4/17/15.
  */
 class KinesisHandler {
-
+  implicit val kinesisClient = Client.fromCredentials(DefaultHomePropertiesFile)
 }
